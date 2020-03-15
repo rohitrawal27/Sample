f2f5608e0a3742c9a8a7b373616968c1339511b4
rohitrawal27 <rohitrawal.net@gmail.com>
2020-03-15 17:12:37 +0530

    First Commit

---
+++ Practice.Web/Practice.Web/Models/RegisterUserViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Practice.Web.Models
+{
+    public class RegisterUserViewModel: RegisterViewModel
+    {
+        public string UserName { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
